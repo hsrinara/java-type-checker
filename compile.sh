9471ce6c115baf8683ca2a2f2ee6584bd074d046
narayanah <narayanah@lmax.com>
2014-11-19 11:15:08 +0000

    Fail if not compiling with java 8.

--- compile.sh
+++ compile.sh
@@ -3,6 +3,18 @@
 BUILD_DIR=build
 LIB_DIR=lib
 
+guarantee_java_8()
+{
+  JAVA_VERSION=`$JAVA_HOME/bin/java -version 2>&1 | grep version`
+  echo $JAVA_VERSION | grep "1.8" > /dev/null
+  if [ $? -ne 0 ]; then
+    echo "Required Java 8, got:" $JAVA_VERSION
+    return 1
+  else
+    return 0
+  fi
+}
+
 print_environment_variables()
 {
   echo "JAVA_HOME is:" $JAVA_HOME
@@ -38,6 +50,7 @@ check_units()
     `find_java_source_files`
 }
 
+guarantee_java_8 &&
 print_environment_variables && 
 initialise && 
 compile_main && 
