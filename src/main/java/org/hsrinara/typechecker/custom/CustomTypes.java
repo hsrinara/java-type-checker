7fe6a14f55b1b52ab125d9d033cd84ec07c82f9e
narayanah <narayanah@lmax.com>
2014-11-19 17:47:56 +0000

    remove code from gh-pages

--- src/main/java/org/hsrinara/typechecker/custom/CustomTypes.java
+++
@@ -1,7 +0,0 @@
-package org.hsrinara.typechecker.custom;
-
-@SuppressWarnings("unsafe")
-public class CustomTypes {
-    public static final @Price long price = (@Price long) 1;
-    public static final @Quantity long quantity = (@Quantity long) 1;
-}
--- src/main/java/org/hsrinara/typechecker/custom/TypeFactory.java
+++
@@ -1,9 +0,0 @@
-package org.hsrinara.typechecker.custom;
-
-public class TypeFactory {
-    public static @Price long price(long price) { return price * CustomTypes.price; }
-
-    public static @Quantity long quantity(long quantity) {
-        return quantity * CustomTypes.quantity;
-    }
-}
